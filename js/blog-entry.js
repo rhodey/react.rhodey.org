88f96bb00ee4178c5eebc0849c37a6161e71bdef
Rhodey Orbits <rhodey@anhonesteffort.org>
2016-09-19 09:27:49 -0500

    use css transitions to animate blog entry loading

--- js/blog-entry.js
+++ js/blog-entry.js
@@ -1,9 +1,12 @@
-var React     = require('react');
-var Helmet    = require('react-helmet');
-var marked    = require('marked');
-var highlight = require('highlight.js');
-var Ajax      = require('./ajax.js');
-var blogidx   = require('./blog-index.js');
+var React       = require('react');
+var CssTransGrp = require('react-addons-css-transition-group');
+var Helmet      = require('react-helmet');
+var marked      = require('marked');
+var highlight   = require('highlight.js');
+var Ajax        = require('./ajax.js');
+var blogidx     = require('./blog-index.js');
+
+var TRANSITION_ENTER_MS = 300;
 
 
 var BlogEntryBox = React.createClass({
@@ -28,25 +31,29 @@ var BlogEntryBox = React.createClass({
     marked.setOptions({highlight: function (code) { return highlight.highlightAuto(code).value; }});
     this.loadMarkdown();
   },
-  renderSkeleton: function() {
+  render: function() {
+    var items = [];
+    if (this.state.__html.length > 0) {
+      items.push((
+        <div>
+          <h1>{this.state.entry.title}</h1>
+          <div className="blogEntryMarkdown" key="hack" dangerouslySetInnerHTML={this.state} />
+        </div>
+      ));
+    }
+
     return (
       <div className="blogEntryBox">
         <Helmet title={this.state.entry.title} />
+        <CssTransGrp
+          transitionName="blogEntry"
+          transitionEnterTimeout={TRANSITION_ENTER_MS}
+          transitionLeaveTimeout={0}
+        >
+          {items}
+        </CssTransGrp>
       </div>
     );
-  },
-  render: function() {
-    if (this.state.__html.length === 0) {
-      return this.renderSkeleton();
-    } else {
-      return (
-        <div className="blogEntryBox">
-          <Helmet title={this.state.entry.title} />
-          <h1>{this.state.entry.title}</h1>
-          <div className="blogEntryMarkdown" dangerouslySetInnerHTML={this.state} />
-        </div>
-      );
-    }
   }
 });
 
